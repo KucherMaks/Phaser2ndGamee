2308d957f5f25215b8c75934763d6d78e3a97a44
KucherMaks <158044443+KucherMaks@users.noreply.github.com>
2024-02-13 19:53:36 +0200

    Update game.js

--- game.js
+++ game.js
@@ -76,7 +76,31 @@ function create() {
 
     // коллайдер гравця та платформ
     this.physics.add.collider(player, platforms);
+
+    //  задання управління
+    cursors = this.input.keyboard.createCursorKeys();
 }
 
-function update() {
+function update() 
+{
+    // саме управління
+    if (cursors.left.isDown) {
+        player.setVelocityX(-160);
+
+        player.anims.play('left', true);
+    }
+    else if (cursors.right.isDown) {
+        player.setVelocityX(160);
+
+        player.anims.play('right', true);
+    }
+    else {
+        player.setVelocityX(0);
+
+        player.anims.play('turn');
+    }
+
+    if (cursors.up.isDown && player.body.touching.down) {
+        player.setVelocityY(-330);
+    }
 }
